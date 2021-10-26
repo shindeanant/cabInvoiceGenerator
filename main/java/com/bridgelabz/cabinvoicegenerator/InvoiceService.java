d01081853e8635be0ecaa315f570a31b21bbb2db
shindeanant <shindeanant1997@gmail.com>
2021-10-26 23:39:01 +0530

    Added: The invoice generator should now take in multiple rides and calculate the aggregate total for all.

---
+++ main/java/com/bridgelabz/cabinvoicegenerator/InvoiceService.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class InvoiceService {
+	private static final double COST_PER_KM = 10;
+	private static final double COST_PER_MIN = 1;
+	private static final double MIN_FARE = 5.0;
+
+	// Created a method to calculate the fare of journey
+	public double CalculateFare(double distance, int time) {
+		double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+		if (totalFare < MIN_FARE) {
+			return MIN_FARE;
+		}
+		return totalFare;
+	}
+
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0.0;
+		for (Ride ride : rides) {
+			totalFare = CalculateFare(ride.getDistance(), ride.getTime());
+		}
+		return totalFare;
+	}
+}
