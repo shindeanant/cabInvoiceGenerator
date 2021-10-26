6304c34a059eb6744747762e9e6b19e7598af9a1
shindeanant <shindeanant1997@gmail.com>
2021-10-26 23:47:03 +0530

    Added:Test case of the invoice generator should now retrun total no of rides, total fare and average fare per ride.

--- test/java/com/bridgelabz/cabinvoicegenerator/InvoiceSreviceTest.java
+++ test/java/com/bridgelabz/cabinvoicegenerator/InvoiceSreviceTest.java
@@ -9,7 +9,7 @@ public class InvoiceSreviceTest {
 		InvoiceService invoiceService = new InvoiceService();
 		double distance = 2.0;
 		int time = 5;
-		double totalFare = invoiceService.CalculateFare(distance, time);
+		double totalFare = invoiceService.calculateFare(distance, time);
 		Assert.assertEquals(25, totalFare, 0);
 	}
 
@@ -18,7 +18,7 @@ public class InvoiceSreviceTest {
 		InvoiceService invoiceService = new InvoiceService();
 		double distance = 0.1;
 		int time = 1;
-		double totalFare = invoiceService.CalculateFare(distance, time);
+		double totalFare = invoiceService.calculateFare(distance, time);
 		Assert.assertEquals(5.0, totalFare, 0);
 	}
 
@@ -26,7 +26,16 @@ public class InvoiceSreviceTest {
 	public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
 		Ride[] rides = { new Ride(2.0, 5), new Ride(5.0, 10), new Ride(0.1, 1), new Ride(20, 60) };
 		InvoiceService invoiceService = new InvoiceService();
-		double totalFare = invoiceService.calculateFare(rides);
+		double totalFare = invoiceService.calculateTotalFare(rides);
 		Assert.assertEquals(260, totalFare, 0);
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1), };
+		InvoiceService invoiceService = new InvoiceService();
+		InvoiceSummary invoiceSummary = invoiceService.calculateFare(rides);
+		InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30.0);
+		Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
 	}
 }
