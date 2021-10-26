6c02c4bba605e5d28be5f608c61ebdca7872fb07
shindeanant <shindeanant1997@gmail.com>
2021-10-26 23:46:31 +0530

    Added:The invoice generator should now retrun total no of rides, total fare and average fare per ride.

--- main/java/com/bridgelabz/cabinvoicegenerator/InvoiceService.java
+++ main/java/com/bridgelabz/cabinvoicegenerator/InvoiceService.java
@@ -5,8 +5,7 @@ public class InvoiceService {
 	private static final double COST_PER_MIN = 1;
 	private static final double MIN_FARE = 5.0;
 
-	// Created a method to calculate the fare of journey
-	public double CalculateFare(double distance, int time) {
+	public double calculateFare(double distance, int time) {
 		double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
 		if (totalFare < MIN_FARE) {
 			return MIN_FARE;
@@ -14,11 +13,20 @@ public class InvoiceService {
 		return totalFare;
 	}
 
-	public double calculateFare(Ride[] rides) {
+	public double calculateTotalFare(Ride[] rides) {
 		double totalFare = 0.0;
 		for (Ride ride : rides) {
-			totalFare = CalculateFare(ride.getDistance(), ride.getTime());
+			totalFare = calculateFare(ride.getDistance(), ride.getTime());
 		}
 		return totalFare;
+
+	}
+
+	public InvoiceSummary calculateFare(Ride[] rides) {
+		double totalFare = 0.0;
+		for (Ride ride : rides) {
+			totalFare += calculateFare(ride.getDistance(), ride.getTime());
+		}
+		return new InvoiceSummary(rides.length, totalFare);
 	}
 }
